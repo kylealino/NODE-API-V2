c450a2eead5ce4f55846adc7f88d56c608534c49
unknown <kylealino@gmail.com>
2023-07-30 09:19:50 +0800

    cors update

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ require('dotenv').config()
 const app = express();
 const MONGO_URL = process.env.MONGO_URL
 const PORT  = process.env.PORT || 3000
-const FRONTEND = process.env.FRONTEND
+// const FRONTEND = process.env.FRONTEND
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-var corsOptions = {
-    origin: FRONTEND,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-app.use(cors(corsOptions))
+// var corsOptions = {
+//     origin: FRONTEND,
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+app.use(cors())
 app.use('/api/product', productRoutes)
 
 //main page /
